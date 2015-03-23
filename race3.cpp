09e215871db5fad039ffcdf55f6f6387f0da7935
Natureal <564374850@qq.com>
2015-03-23 21:15:24 +0800

    2015-03-22

--- race3.cpp
+++ race3.cpp
@@ -28,8 +28,85 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> pii;
 const int INF = (1 << 30) - 1;
+const int MAXN = 210;
 
+int g[MAXN][MAXN];
+int vis[MAXN],st,ed;
+vector<int> ans1,ans2;
 
+bool Check1(int p){
+	queue<int> Q;
+	while(!Q.empty()) Q.pop();
+	Q.push(st);
+	MEM(vis,0);
+	vis[st] = 1;
+	while(!Q.empty()){
+		int x = Q.front(); Q.pop();
+		for(int i = st + 1; i <= ed; ++i) if(g[x][i]){
+			if(i == p) continue;
+			if(!vis[i]){
+				vis[i] = 1;
+				Q.push(i);
+			}
+		}
+	}
+	return vis[ed] == 0;
+}
+
+int fa[MAXN];
+
+bool Check2(int p){
+	queue<int> Q;
+	while(!Q.empty()) Q.pop();
+	MEM(vis,0);
+	vis[p] = 1;
+	for(int i = st; i <= ed; ++i) if(!vis[i] && g[p][i]){
+		vis[i] = 1;
+		Q.push(i);
+	}
+	while(!Q.empty()){
+		int x = Q.front(); Q.pop();
+		for(int i = st; i <= ed; ++i) if(g[x][i] || g[i][x]){
+			if(i == p) continue;
+			if(!vis[i]){
+				vis[i] = 1;
+				Q.push(i);
+			}
+		}
+	}
+	for(int i = st; i <= ed; ++i) if(vis[i] == 0) return true;
+	return false;
+}
 
 int main(){
+	freopen("race3.in","r",stdin);
+	freopen("race3.out","w",stdout);
+	int tot = 0;
+	int a,b;
+	while(scanf("%d",&a) != EOF){
+		if(a == -1) break;
+		if(a == -2){
+			tot++;
+			continue;
+		}
+		g[tot][a] = 1;
+		while(cin >> a && a != -2) g[tot][a] = 1;
+		tot++;
+	}
+	st = 0;
+	ed = tot - 1;
+	REP(i,ed - 1){
+		if(Check1(i)){
+			ans1.push_back(i);
+			if(Check2(i)) ans2.push_back(i);
+		}
+	}
+	printf("%d",(int)ans1.size());
+	for(int i = 0; i < ans1.size(); ++i) printf(" %d",ans1[i]);
+	puts("");
+	printf("%d",(int)ans2.size());
+	for(int i = 0; i < ans2.size(); ++i) printf(" %d",ans2[i]);
+	puts("");
+	return 0;
+}
 
---
+++ shuttle.cpp
@@ -0,0 +1,76 @@
+/*
+ID:naturec1
+PROG: shuttle
+LANG: C++
+*/
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MEM(a,b) memset(a,b,sizeof(a))
+#define REP(i,n) for(int i=1;i<=(n);++i)
+#define REV(i,n) for(int i=(n);i>=1;--i)
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define RFOR(i,a,b) for(int i=(a);i>=(b);--i)
+#define getmid(l,r) ((l) + ((r) - (l)) / 2)
+#define MP(a,b) make_pair(a,b)
+#define PB(a) push_back(a)
+
+typedef long long ll;
+typedef pair<int,int> pii;
+const int INF = (1 << 30) - 1;
+
+int N;
+vector<int> ans1,ans2;
+
+int main(){
+	freopen("shuttle.in","r",stdin);
+	freopen("shuttle.out","w",stdout);
+	scanf("%d",&N);
+	if(N == 1){
+		printf("1 3 2\n");
+		return 0;
+	}
+	ans1.PB(2); ans1.PB(1);
+	int f = -1;
+	for(int i = 1; i <= N - 2; ++i){
+		for(int j = 1; j <= i + 1; ++j) ans1.PB(2 * f);
+		ans1.PB(f);
+		f *= -1;
+	}
+	for(int i = 1; i <= N; ++i) ans1.PB(2 * f);
+	f = -1;
+	ans2.PB(-1); ans2.PB(2); ans2.PB(1);
+	for(int i = 1; i <= N - 2; ++i){
+		for(int j = 1; j <= i + 1; ++j) ans2.PB(2 * f);
+		ans2.PB(f);
+		f *= -1;
+	}
+	int st = N;
+	int tot = 1;
+	printf("%d",st);
+	for(int i = 0; i < ans1.size(); ++i){
+		st += ans1[i];
+		if(tot % 20) printf(" ");
+		printf("%d",st);
+		if((++tot) % 20 == 0) puts("");
+	}
+	for(int i = ans2.size() - 1; i >= 0; --i){
+		st += ans2[i];
+		if(tot % 20) printf(" ");
+		printf("%d",st);
+		if((++tot) % 20 == 0) puts("");
+	}
+	if(tot % 20) puts("");
+	return 0;
+}
